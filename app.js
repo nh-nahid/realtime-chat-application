e8029973757ab0eec09f43a8f0643272b061311f
Nahid Hossain <nahid4510@gmail.com>
2026-06-16 00:18:00 +0600

    default error handler

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+// external imports
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+
+// internal imports
+const { notFoundHandler, errorHandler } = require('./middlewares/common/errorHandler');
 
 const app = express();
 
 dotenv.config();
 
 // database connection
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("database onnection successful!"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+    .then(() => console.log("database onnection successful!"))
+        .catch(err => console.log(err));
 
 // request parsers
 app.use(express.json());
@@ -25,6 +28,17 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 // parse cookies
-app.listen(3000, () => {
-    console.log("app is listening at port 3000");
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+
+
+// 404 not found handler
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+    console.log(`app is listening at port ${process.env.PORT}`);
 })
