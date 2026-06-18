16b97bbee882178879331967629d4d0415136c53
Nahid Hossain <nahid4510@gmail.com>
2026-06-18 22:23:13 +0600

    login & logout feature implemented

---
+++ middlewares/common/checkLogin.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+const checkLogin = (req, res, next) => {
+    let cookies = Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
+
+    if(cookies) {
+        try {
+            token = cookies[process.env.COOKIE_NAME];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decoded;
+
+            // pass user info to response locals
+            if(res.locals.html) {
+                res.locals.loggedInUser = decoded;
+            }
+
+            next();
+        } catch (error) {
+            if(res.locals.html) {
+                res.redirect("/");
+            } else {
+                res.status(500).json({
+                    errors: {
+                        common: {
+                            msg: "Authentication failure!"
+                        }
+                    }
+                })
+            }
+        }
+    }
+}
+
+module.exports = checkLogin;
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,13 +2,23 @@
 const express = require("express");
 
 // internal imports
-const { getLogin } = require('../controller/loginController');
+const { getLogin, login, logout } = require('../controller/loginController');
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
+const { doLoginValidators, doLoginValidationHandler } = require('../middlewares/login/loginValidators');
 
 const router = express.Router();
 
+// set page title
+const page_title = 'Login'
+
 // login page
-router.get('/', decorateHtmlResponse("Login"), getLogin);
+router.get('/', decorateHtmlResponse(page_title), getLogin);
+
+// process login
+router.post('/', decorateHtmlResponse(page_title), doLoginValidators, doLoginValidationHandler, login);
+
+// logout
+router.delete("/", logout);
 
 
 module.exports = router;
